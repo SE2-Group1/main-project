96b0cf4e0e3d851402bc707c816cc2ec9a8a54e8
Emanuele Coricciati <emanuelecoricciati@libero.it>
2024-11-21 06:36:19 +0100

    add docker for production

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -4,7 +4,7 @@ require('dotenv').config({ path: '../.env' });
 
 const db = new Pool({
   user: process.env.DB_USER, // Database username
-  host: 'localhost', // Database host
+  host: process.env.NODE_ENV === 'prod' ? 'db' : 'localhost', // Database host
   database: process.env.DB_NAME, // Database name
   password: process.env.DB_PASSWORD, // Database password
   port: 5432, // Database port (default for PostgreSQL)
