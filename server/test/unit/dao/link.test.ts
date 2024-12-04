9cb59b3379ae3ba3301b87eb50d4b2bdddc6b2c9
heymitra <heydarimitra98@gmail.com>
2024-12-04 13:57:51 +0100

    solve issue on unit test

--- server/test/unit/dao/link.test.ts
+++ server/test/unit/dao/link.test.ts
@@ -81,7 +81,7 @@ describe('Link DAO', () => {
         });
 
       const result = await linkDAO.getLinks(1);
-      expect(result).toEqual([new Link('2', 'linkType')]);
+      expect(result).toEqual([new Link('2', 2, 'linkType')]);
       mockDBQuery.mockRestore();
     });
 
